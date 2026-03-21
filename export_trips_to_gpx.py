f4a26dddfd16438667dfb79d1458c30b6fbf4cf0
Vladimir Ryabtsev <vryabtsev@news360.com>
2026-03-21 02:25:59 -0700

    Saving to GPX

--- export_trips_to_gpx.py
+++ export_trips_to_gpx.py
@@ -1,6 +1,7 @@
 import sys
 from pathlib import Path
 from tqdm import tqdm
+from gpx import GPX, Metadata, Track, TrackSegment, Waypoint, Extensions
 from redtiger.filesystem import split_trips, timestamp_from_filename
 from redtiger.telemetry import Packet
 from redtiger.parsing import extract_telemetry_from_mp4
@@ -24,18 +25,19 @@ def main():
 
     export_tracks(videos_path, gpx_path)
 
-def export_tracks(videos_path: Path, gpx_path: Path):
-    trips = split_trips(videos_path)
+def export_tracks(video_path: Path, gpx_path: Path):
+    if video_path.is_file():
+        trips = [[video_path]]
+    else:
+        trips = split_trips(video_path)
+        print(f'{sum(len(trip) for trip in trips)} videos were combined into {len(trips)} trips')
+
     pbar = tqdm(trips)
     for trip in pbar:
-        trip_start = timestamp_from_filename(trip[0].name)
-        trip_end = timestamp_from_filename(trip[-1].name)
-        pbar.set_description(f'Processing trip {trip_start.strftime('%H:%M')} – {trip_end.strftime('%H:%M')} from {trip_start.strftime('%b %-d')}')
-        export_track(trip, gpx_path)
+        pbar.set_description(f'Processing trip {get_trip_name(trip)}')
+        export_track_data(trip, gpx_path)
 
-    print(f'{sum(len(trip) for trip in trips)} videos were combined into {len(trips)} trips')
-
-def export_track(trip: list[Path], gpx_path: Path):
+def export_track_data(trip: list[Path], gpx_path: Path):
     trip_data = []
     pbar = tqdm(trip, leave=False)
     for item in pbar:
@@ -43,10 +45,64 @@ def export_track(trip: list[Path], gpx_path: Path):
         trip_data += extract_telemetry_from_mp4(item)
     
     track_path = gpx_path / (trip[0].stem + '.gpx')
-    save_to_gpx_track(trip_data, track_path)
+    save_to_gpx_track(trip_data, get_trip_name(trip), track_path)
 
-def save_to_gpx_track(telemetry: list[Packet], save_path: Path):
-    pass
+def save_to_gpx_track(telemetry: list[Packet], trip_name, save_path: Path):
+    import xml.etree.ElementTree as ET
+    from datetime import datetime, UTC
+    from gpx import Waypoint, Extensions
+
+    REDTIGER_DASHCAM_TPX = "https://github.com/greatvovan/redtiger-dashcam/xmlschemas/TrackPointExtension/v1"
+    ET.register_namespace("rd", REDTIGER_DASHCAM_TPX)
+
+    points = []
+    for t in telemetry:
+        tpx = ET.Element(f"{{{REDTIGER_DASHCAM_TPX}}}TrackPointExtension")
+        speed = ET.SubElement(tpx, f"{{{REDTIGER_DASHCAM_TPX}}}speed_kmh")
+        speed.text = str(round(t.speed_kmh, 2))
+        bearing = ET.SubElement(tpx, f"{{{REDTIGER_DASHCAM_TPX}}}bearing_deg")
+        bearing.text = str(round(t.bearing, 2))
+        nmea_status = ET.SubElement(tpx, f"{{{REDTIGER_DASHCAM_TPX}}}nmea_status")
+        nmea_status.text = t.nmea_status
+        gx = ET.SubElement(tpx, f"{{{REDTIGER_DASHCAM_TPX}}}gx")
+        gx.text = str(t.gx)
+        gy = ET.SubElement(tpx, f"{{{REDTIGER_DASHCAM_TPX}}}gy")
+        gy.text = str(t.gy)
+        gz = ET.SubElement(tpx, f"{{{REDTIGER_DASHCAM_TPX}}}gz")
+        gz.text = str(t.gz)
+
+        trkpt = Waypoint(
+            lat=round(t.latitude, 6),
+            lon=round(t.longitude, 6),
+            time=t.utc_timestamp,
+            ele=0.0,
+            extensions=Extensions(elements=[tpx])
+        )
+        points.append(trkpt)
+
+    segment = TrackSegment(trkpt=points)
+    track = Track(trkseg=[segment])
+
+    metadata = Metadata(
+        name='Road trip ' + trip_name,
+        desc='RedTiger dash camera GPS track',
+        time=datetime.now(UTC),
+    )
+
+    gpx = GPX(
+        creator="Redtiger-dashcam (home on GitHub)",
+        metadata=metadata,
+        trk=[track],
+    )
+
+    with open(save_path, 'wt') as file:
+        file.write(gpx.to_string())
+
+def get_trip_name(trip: list) -> str:
+    trip_start = timestamp_from_filename(trip[0].name)
+    trip_end = timestamp_from_filename(trip[-1].name)
+    return f'{trip_start.strftime('%H:%M')} – {trip_end.strftime('%H:%M')} ' \
+           f'from {trip_start.strftime('%b %-d')}'
 
 
 if __name__ == '__main__':
